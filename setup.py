fba686c29ad01502020540244b048fb670d65905
Ruan Pearce-Authers <ruanpa@outlook.com>
2020-08-07 21:08:07 +0100

    Add description to PyPI packaging

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("readme.md", "r") as fh:
+	long_description = fh.read()
+
 setup(
 	name = 'govyn',
-	version = '0.1.0',
+	version = '0.1.1',
 	author = 'Ruan Pearce-Authers',
 	author_email = 'ruanpa@outlook.com',
 	description = 'HTTP APIs in typed Python',
 	url = 'https://github.com/returnString/govyn',
+	long_description = long_description,
+	long_description_content_type = 'text/markdown',
 	packages = find_packages(),
 	package_data = {
 		'govyn': [ 'py.typed' ],
@@ -19,9 +24,9 @@ setup(
 		'requests >= 2.24',
 	],
 	classifiers=[
-		"Programming Language :: Python :: 3",
-		"License :: OSI Approved :: MIT License",
-		"Operating System :: OS Independent",
+		'Programming Language :: Python :: 3',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
 	],
 	python_requires = '>= 3.8',
 )
