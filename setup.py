eaf3c026b46ca9085319ba42b771ab2239e1184d
Ruan Pearce-Authers <ruanpa@outlook.com>
2020-08-07 20:17:09 +0100

    Testing PyPI release

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
 	name = 'govyn',
+	version = '0.1.0',
+	author = 'Ruan Pearce-Authers',
+	author_email = 'ruanpa@outlook.com',
 	description = 'HTTP APIs in typed Python',
-	packages = [ 'govyn' ],
+	url = 'https://github.com/returnString/govyn',
+	packages = find_packages(),
 	package_data = {
 		'govyn': [ 'py.typed' ],
 	},
@@ -12,6 +16,12 @@ setup(
 		'starlette >= 0.13',
 		'uvicorn >= 0.11',
 		'dacite >= 1.5.1',
-		'requests >= 2.24'
-	]
+		'requests >= 2.24',
+	],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires = '>= 3.8',
 )
