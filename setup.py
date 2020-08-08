8cdef369bdea93526f279446beffc9240a9e7b41
returnString <returnString@users.noreply.github.com>
2020-08-08 14:26:39 +0000

    Release version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('readme.md', 'r', encoding = 'utf-8') as fh:
 
 setup(
 	name = 'govyn',
-	version = '0.1.4',
+	version = '0.1.6',
 	author = 'Ruan Pearce-Authers',
 	author_email = 'ruanpa@outlook.com',
 	description = 'HTTP APIs in typed Python',
