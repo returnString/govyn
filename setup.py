24534be9d644a170fa02702fb972092b4b6cbbba
Ruan Pearce-Authers <ruanpa@outlook.com>
2020-08-08 12:17:29 +0100

    Ship v0.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('readme.md', 'r', encoding = 'utf-8') as fh:
 
 setup(
 	name = 'govyn',
-	version = '0.1.3',
+	version = '0.1.4',
 	author = 'Ruan Pearce-Authers',
 	author_email = 'ruanpa@outlook.com',
 	description = 'HTTP APIs in typed Python',
